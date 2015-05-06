6acd1729a65407a1343fbeb8706d80555247683f
Haoyan Huo <hhaoyann@gmail.com>
2015-05-06 19:16:05 +0800

    add index for database

--- project/tuples_app/models.py
+++ project/tuples_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Relation(models.Model):
-     subject = models.CharField(max_length=255)
-     verb = models.CharField(max_length=255)
+     subject = models.CharField(max_length=255, db_index=True)
+     verb = models.CharField(max_length=255, db_index=True)
      target = models.TextField()
