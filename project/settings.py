96b77cbe27835656e6c9d0c111b66ade35e1eca7
Haoyan Huo <hhaoyann@gmail.com>
2015-05-07 10:03:23 +0800

    add debug database

--- project/settings.py
+++ project/settings.py
@@ -1,13 +1,24 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nlp_project',
-        'USER': 'nlp_project',
-        'PORT': 17806,
-        'PASSWORD': 'nlp_project',
-        'HOST': '127.0.0.1',
+import os
+
+if os.environ.has_key('NLP_DEBUG'):
+    print 'Enter debug mode for database.'
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'nlp_project.db'
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'nlp_project',
+            'USER': 'nlp_project',
+            'PORT': 17806,
+            'PASSWORD': 'nlp_project',
+            'HOST': '127.0.0.1',
+        }
+    }
 
 SECRET_KEY = '_mw2k_abk#x97bc#_w*fb%&k#uw9_3*63@3kzl(2hb)!lbi8j$'
 INSTALLED_APPS = ['bdbk']
