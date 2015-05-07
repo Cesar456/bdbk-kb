8826ef653afad87d78ec92ee18a79d1d9b0646d6
Haoyan Huo <hhaoyann@gmail.com>
2015-05-07 14:39:54 +0800

    add compare routine between two strings

--- project/merge_project.py
+++ project/merge_project.py
@@ -4,25 +4,33 @@
 from unicode_classifier import is_good_char
 import jieba
 
-def is_identity_content(str1, str2, coef=0.7):
+def is_identity_content(str1, str2, coef=0.9):
     '''Determine whether two strings are similar,
     using bags of word assumption'''
 
     lst1 = set(x for x in jieba.cut(str1, cut_all=False) if is_good_char(x))
     lst2 = set(x for x in jieba.cut(str2, cut_all=False) if is_good_char(x))
 
-    print 'lst1:', ','.join(lst1)
-    print 'lst2:', ','.join(lst2)
-    print 'common:', ','.join(lst1 & lst2)
-    
+    #print 'lst1:', ','.join(lst1)
+    #print 'lst2:', ','.join(lst2)
+    #print 'common:', ','.join(lst1 & lst2)
+
     common_length = len(lst1 & lst2)
-    minimal_length = min(len(lst1), len(lst2))
-    if common_length >= coef * minimal_length:
+    avg_length = (len(lst1) + len(lst2)) / 2
+    if common_length >= coef * avg_length and common_length > 0:
         return True
     else:
         return False
 
 if __name__ == '__main__':
-    s1 = u'浙江省绍兴府会稽县'
-    s2 = u'浙江省绍兴府会稽县 (今浙江省绍兴市绍兴县)'
-    print is_identity_content(s1, s2)
+    clst1 = open('zhwiki_relation.csv').read().decode('utf8').splitlines()
+    clst2 = open('bdbk_relation.csv').read().decode('utf8').splitlines()
+
+    print 'Guessed identical sentences:'
+    for i in clst1:
+        for j in clst2:
+            if is_identity_content(i,j):
+                print '-' * 20
+                print i
+                print j
+                #print is_identity_content(i, j)
