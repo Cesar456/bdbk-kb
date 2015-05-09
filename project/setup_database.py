a7937118d0025d5b281d7c69679347a6812c45db
Haoyan Huo <hhaoyann@gmail.com>
2015-05-09 10:10:48 +0800

    change baidu baike database schema

--- project/setup_database.py
+++ project/setup_database.py
@@ -6,7 +6,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from django.db import models
 from bdbk.models import Verb as BaiduVerb
 from bdbk.models import NamedEntity as BaiduNamedEntity
-from bdbk.models import Relation as BaiduRelation
+from bdbk.models import InfoboxTuple as BaiduInfoboxTuple
+from bdbk.models import NamedEntityRedirect as BaiduNamedEntityRedirect
+
 from zhwiki.models import Verb as ZhWikiVerb
 from zhwiki.models import NamedEntity as ZhWikiNamedEntity
 from zhwiki.models import Relation as ZhWikiRelation
