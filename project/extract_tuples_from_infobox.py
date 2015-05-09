1ea4b0b95a535f9d911598ab2a8ec1c8eb3fd2c5
Haoyan Huo <hhaoyann@gmail.com>
2015-05-09 10:11:01 +0800

    modify extractor code

--- project/extract_tuples_from_infobox.py
+++ project/extract_tuples_from_infobox.py
@@ -118,28 +118,26 @@ class Extractor(object):
             abstract = self.get_abstract(page)
             tuples = self.get_tuples(page)
 
-            print 'A real page'
-            print title, search_term
-            print abstract
-            for i in tuples:
-                print '\t'.join(i)
+            return (title, search_term, abstract, tuples)
         else:
             lemmas = self.get_lemma_list(page)
 
-            print 'A lemma list'
-            print title
-            print ';'.join(lemmas)
+            return (title, lemmas)
 
 extractor = Extractor()
 
 if __name__ == '__main__':
     import argparse
     from baidu_database import BaiduDatabase
+    import setup_database
+    from bdbk.models import *
+    from django.db import connection
+    import time
 
     parser = argparse.ArgumentParser(
         description='I will read baidu baike and produce you the info tuples of its infobox.')
     parser.add_argument('--dir', required=True, help='database dir.')
-    parser.add_argument('--db-name', help='name of the database.')
+    parser.add_argument('--db-name', required=True, help='name of the database.')
     parser.add_argument('--log', required=True, help='log file name.')
 
     args = parser.parse_args()
@@ -147,16 +145,59 @@ if __name__ == '__main__':
     db_name = args.db_name
     log_fn = args.log
 
+    from setup_logging import setup as setup_logger
+    logging = setup_logger(log_fn)
     logging.info('Source: %s in %s', db_name, dir)
 
     db = BaiduDatabase(dir, db_name)
-    for pid, ptitle, pcontent in db.pages():
+    verb_dict = {}
+
+    processed_count = 0
+    last_time = time.time()
+    last_processed = 0
+
+    for pid, ptitle, pcontent in db.all_pages():
+        processed_count += 1
+        if processed_count % 10 == 0:
+            logging.info('%d processed in %d/sec', processed_count, (processed_count-last_processed)/(time.time()-last_time))
+            last_processed = processed_count
+            last_time = time.time()
+
         try:
-            extractor.extract(pid, pcontent)
+            info = extractor.extract(pid, pcontent)
+            if len(info) == 4:
+                title, search_term, abstract, tuples = info
+                if len(tuples) > 0:
+                    ne = NamedEntity(name=title, 
+                        search_term=search_term,
+                        abstract=abstract,
+                        page_id=pid)
+                    ne.save()
+
+                    npk = ne.pk
+                    for tuple in tuples:
+
+                        if tuple[0] not in verb_dict:
+                            v, _ = Verb.objects.get_or_create(name=tuple[0])
+                            verb_dict[tuple[0]] = v.pk
+                            vpk = v.pk
+                        else:
+                            vpk = verb_dict[tuple[0]]
+
+                        t = InfoboxTuple(named_entity_id=npk,
+                            content=tuple[1],
+                            verb_id=vpk)
+                        t.save()
+
+            elif len(info) == 2:
+                title, lemmas = info
+                for lemma in lemmas:
+                    ner = NamedEntityRedirect(page_id=pid,
+                        name=title,
+                        linked_name=lemma)
+                    ner.save()
+
         except Exception as e:
-            logging.warning('exception %r: page_id(%d)', e, page_id)
+            logging.warning('exception %r: page_id(%d)', e, pid)
 
     db.close()
-else:
-    import sys
-    extractor.extract(0, open(sys.argv[1]).read())
