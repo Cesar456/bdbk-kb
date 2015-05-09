00c692d47165a9766af708ce346d13de359189bc
Haoyan Huo <hhaoyann@gmail.com>
2015-05-09 10:10:24 +0800

    change baidu baike database schema

--- project/bdbk/models.py
+++ project/bdbk/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
 class Verb(models.Model):
-    name = models.CharField(max_length=255, db_index=True)
+    name = models.CharField(max_length=255, db_index=True, unique=True)
 
 class NamedEntity(models.Model):
-    name = models.CharField(max_length=255, db_index=True)
+    '''
+    page_id is the baidu baike page id.
+    '''
 
-class Relation(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    search_term = models.CharField(max_length=255, db_index=True)
+    abstract = models.TextField()
+    page_id = models.IntegerField()
+
+class InfoboxTuple(models.Model):
     named_entity = models.ForeignKey('NamedEntity', db_index=True)
     verb = models.ForeignKey('Verb', db_index=True)
     content = models.TextField()
+
+class NamedEntityRedirect(models.Model):
+    '''
+    page_id is the baidu baike page id.
+    named_entities is a list of named_entities separated by comma.
+    '''
+
+    page_id = models.IntegerField()
+    name = models.CharField(max_length=255, db_index=True)
+    linked_name = models.CharField(max_length=255, db_index=True)
