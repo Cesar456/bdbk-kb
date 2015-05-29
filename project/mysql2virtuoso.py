d16effd99847170b401066e276e1e482ead0cf51
Haoyan Huo <hhaoyann@gmail.com>
2015-05-29 11:00:25 +0800

    turn into txt file

--- project/mysql2virtuoso.py
+++ project/mysql2virtuoso.py
@@ -3,11 +3,6 @@
 
 import re
 
-from django.core.paginator import Paginator
-from django.db import connection
-
-import project.setup_database
-from bdbk.models import NamedEntity
 from virtuoso import sparql
 from project.setup_logging import setup as setup_logging
 
@@ -30,43 +25,19 @@ def clear_graph():
     sparql_connection.execute('CLEAR GRAPH <http://baike.baidu.com>')
     return 'graph cleared'
 
-def insert_tuples(logging):
-    cursor = connection.cursor()
-
-    logging.info('couting objects...')
-
-    cursor.execute('''
-        SELECT COUNT(*) FROM bdbk_infoboxtuple
-        ''')
-    total_count = cursor.fetchall()[0][0]
-
-    query = '''
-        SELECT bdbk_infoboxtuple.content, 
-            bdbk_namedentity.page_id, 
-            bdbk_verb.name 
-        FROM bdbk_infoboxtuple 
-        INNER JOIN bdbk_namedentity 
-            ON bdbk_infoboxtuple.named_entity_id = bdbk_namedentity.id 
-        INNER JOIN bdbk_verb 
-            on bdbk_infoboxtuple.verb_id = bdbk_verb.id 
-        LIMIT %d,%d
-        '''
-
-    row_size = 1000
-    regx = re.compile(r'\{\{link:(.*?)\|(.*?)\}\}')
-    baike_url_prefix = 'http://baike.baidu.com'
-
-    for offset in range(0, total_count, row_size):
-
-        logging.info('fetching objects from database (%d/%d)...', offset, total_count)
-
-        cursor.execute(query % (offset, row_size))
-        rows = cursor.fetchall()
+def insert_tuples(triple_fn, logging):
+    def process_lines(lines, start_line):
         subjects = []
         predicts = []
         objects = []
-        for row in rows:
-            content, page_id, predict = row
+
+        regx = re.compile(r'\{\{link:(.*?)\|(.*?)\}\}')
+        baike_url_prefix = 'http://baike.baidu.com'
+
+        for line in lines:
+            line = line.rstrip()
+            page_id, predict, content = line.split('\t')
+
             match = re.match(regx, content)
             if match is not None:
                 object = sparql.SparQLURI(baike_url_prefix+match.group(1))
@@ -76,33 +47,49 @@ def insert_tuples(logging):
                         lambda x:'{{link:%s|%s}}' % (baike_url_prefix+x.group(1), x.group(2)),
                         content))
 
-            subjects.append(sparql.SparQLURI('http://baike.baidu.com/view/%d.htm' % page_id))
+            subjects.append(sparql.SparQLURI('http://baike.baidu.com/view/%s.htm' % page_id))
             predicts.append(sparql.SparQLURI(PREDICT_PREDICT_PREFIX + predict))
             objects.append(object)
-    
+
         try:
             sparql_connection.batch_insert(subjects, predicts, objects)    
         except Exception as e:
-            logging.error('exception at offset %d: %r', offset, e)
+            logging.error('exception at block %d(start line): %r', start_line, e)
         else:
-            logging.info('processed %d tuples', len(rows))
+            logging.info('(tuple):processed %d/%d(start line) tuples', len(lines), start_line)
 
-def insert_nes(logging):
-    paginator = Paginator(NamedEntity.objects.all(), 500)
-    logging.info('couting objects...')
-    for page in range(1, paginator.num_pages + 1):
+    triple_file = open(triple_fn)
+    row_size = 500
+
+    line_number = 0
+    start_line = 1
+    lines = []
+
+    for i in triple_file:
+        lines.append(i)
+
+        line_number += 1
+        if line_number % row_size == 0:
+            process_lines(lines, start_line)
+            lines = []
+            start_line = line_number
+
+    if len(lines) > 0:
+        process_lines(lines, start_line)
+
+    triple_file.close()
+
+def insert_nes(ne_fn, logging):
+    def process_lines(lines, start_line):
         subjects = []
         predicts = []
         objects = []
 
-        logging.info('fetching objects from database (%d/%d)...', page, paginator.num_pages)
+        for line in lines:
+            line = line.rstrip()
+            page_id, title, search_term = line.split('\t')
 
-        for i in paginator.page(page).object_list:
-            search_term = i.search_term
-            title = i.name
-            page_id = i.page_id
-
-            url = 'http://baike.baidu.com/view/%d.htm' % page_id
+            url = 'http://baike.baidu.com/view/%s.htm' % page_id
             subjects.append(sparql.SparQLURI(url))
             predicts.append(sparql.SparQLURI(PREDICT_SEARCH_TERM))
             objects.append(sparql.SparQLLiteral(search_term))
@@ -112,11 +99,32 @@ def insert_nes(logging):
             objects.append(sparql.SparQLLiteral(title))
 
         try:
-            sparql_connection.batch_insert(subjects, predicts, objects)
+            sparql_connection.batch_insert(subjects, predicts, objects)    
         except Exception as e:
-            logging.error('failed at page: %d: %r', page, e)
+            logging.error('exception at block %d(start line): %r', start_line, e)
         else:
-            logging.info('inserted %d pages', len(paginator.page(page).object_list))
+            logging.info('(ne):processed %d/%d(start line) tuples', len(lines), start_line)
+
+    ne_file = open(ne_fn)
+    row_size = 250
+
+    line_number = 0
+    start_line = 1
+    lines = []
+
+    for i in ne_file:
+        lines.append(i)
+
+        line_number += 1
+        if line_number % row_size == 0:
+            process_lines(lines, start_line)
+            lines = []
+            start_line = line_number
+
+    if len(lines) > 0:
+        process_lines(lines, start_line)
+
+    ne_file.close()
 
 if __name__ == '__main__':
     import argparse
@@ -124,15 +132,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Dump all triples from mysql to virtuoso.')
     parser.add_argument('--log', required=True, help='log file name.')
-    parser.add_argument('--clear', type=bool, help='clear the database.')
+    parser.add_argument('--tuple-file', required=True, help='triple dump from database.')
+    parser.add_argument('--ne-file', required=True, help='ne dump from database.')
     args = parser.parse_args()
     log_fn = args.log
-    clear = args.clear
+    tuple_file = args.tuple_file
+    ne_file = args.ne_file
 
     logging = setup_logging(log_fn)
 
     if clear:
         clear_graph()
-    insert_nes(logging)
-    insert_tuples(logging)
+    insert_nes(ne_file, logging)
+    insert_tuples(tuple_file, logging)
 
