a297b620b3e3f8f011daa1dba9441e27a5aa4f28
Haoyan Huo <hhaoyann@gmail.com>
2015-07-07 09:17:22 +0800

    category graph viewer

--- wikipedia/build_graph.py
+++ wikipedia/build_graph.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
-
+# -*- coding:utf-8 -*-
 import json
 
 class CategoryGraph(object):
-    def __init__(self, category_fn, category_link_fn):
+    def __init__(self, category_fn, category_link_fn, hiddencat_fn):
         self.cat__name_id = {}
         self.cat__id_name = {}
         self.graph_inlink = {}
         self.graph_outlink = {}
+        self.stop_cats = []
 
         # read all cat names
         for cat in json.loads(open(category_fn).read()):
@@ -41,6 +42,12 @@ class CategoryGraph(object):
 
         print 'Inlink graph imported.'
 
+        # read hidden cats(stop cats)
+        for hidden_cats in json.loads(open(hiddencat_fn).read()):
+            self.stop_cats.append(hidden_cats['title'])
+
+        print 'Stop category(hidden category) list imported.'
+
         # build outlink graph
         print 'Building outlink graph...',
         total_size = len(self.graph_inlink)
@@ -61,7 +68,7 @@ class CategoryGraph(object):
                 print ',%%%d' % (counter*100/total_size),
 
         print 'Done'
-        print '%d categories, %d links' % (len(self.cat__id_name), link_counter)
+        print '%d categories(%d hidden), %d links' % (len(self.cat__id_name), len(self.stop_cats), link_counter)
 
     def outlink_name(self, cat_title):
         pageId = self.cat__name_id[cat_title]
@@ -93,6 +100,49 @@ class CategoryGraph(object):
             for link in links:
                 yield link
 
-graph = CategoryGraph('Category.json', 'category_inlinks.json')
+# Category.json: list of dict: {'id':'...', 'pageId':'...', 'name': '...'}
+# category_inlinks.json: list of dict: {'id':'...', 'inLinks':'...'}
+# hiddencats_without_id.json: list of dict: {'title':'...', 'url':'...(not used)'}
+graph = CategoryGraph('Category.json', 'category_inlinks.json', 'hiddencats_without_id.json')
+
+def browser(graph, current=None):
+    def select(lst, help):
+        for i in range(0, len(lst)):
+            outlink = lst[i]
+            print '[%d]: %s(%d)' % (i, graph.cat__id_name[outlink], outlink)
+
+        print '\n' + help
+
+        s = raw_input()
+        if not s:
+            return -1
+        else:
+            return int(s)
+
+    current = 254517 if not current else current
+    print 'Welcome'
+    while True:
+        outlinks = graph.graph_outlink[current]
+        print 'Current category: %s(%d), with %d children' % (graph.cat__id_name[current], current, len(outlinks))
+        cmd = select(outlinks, '%d-%d to select, 0 to go up' % (0, len(outlinks)-1))
+
+        oldcurrent = current
+        if cmd >= 0 and cmd < len(outlinks):
+            current = outlinks[cmd]
+            if current not in graph.graph_outlink:
+                print '%s(%d) has no child\n' % (graph.cat__id_name[current], current)
+                current = oldcurrent
+        else:
+            current=graph.graph_inlink[current]
+            if len(current) == 1:
+                current = current[0]
+            else:
+                current = current[select(current, '%d-%d to select where to go' % (0, len(current)-1))]
+
+            if current not in graph.graph_outlink:
+                print '%s(%d) has no child\n' % (graph.cat__id_name[current], current)
+                current = oldcurrent
+
+browser(graph)
 import code
 code.interact(local=locals())
