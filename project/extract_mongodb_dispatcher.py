6bb27ade8a63a373b6117427bd740f20a4d8b751
Haoyan Huo <hhaoyann@gmail.com>
2015-07-08 16:32:46 +0800

    mongodb dispatcher

---
+++ project/extract_mongodb_dispatcher.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import logging
+import subprocess
+import threading
+
+import pymongo
+import threadpool
+
+if __name__  == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='parallel process mongodb baidu baike data.')
+    parser.add_argument('--mongod-host', type=str, required=True, help='host of mongo db server.')
+    parser.add_argument('--mongod-port', type=int, required=True, help='port of mongo db server.')
+    parser.add_argument('--worker-dir', type=str, required=True, help='work directory for workers(temporary files may be stored there).')
+    parser.add_argument('--worker-job-count', type=int, default=100000, help='how many pages should a worker work on.')
+    parser.add_argument('--worker-count', type=int, default=10, help='how many workers should be there.')
+
+    args = parser.parse_args()
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.DEBUG)
+
+    def get_doc_count():
+        client = pymongo.MongoClient(args.mongod_host, args.mongod_port)
+        c = client.baidu.data.find().count()
+        client.close()
+        return c
+
+    def process(params):
+        slice_from, slice_to = params
+        worker_id = str(threading.current_thread().ident)
+
+        logging.info('worker %s started, working on doc %d-%d', worker_id, slice_from, slice_to)
+
+        local_log_fn = 'worker-%s-%d-%d.log' % (worker_id, slice_from, slice_to)
+        subprocess.call(['python', 'extract_tuples_from_infobox.py',
+                         '--src', 'mongodb',
+                         '--mongod-host', args.mongod_host,
+                         '--mongod-port', str(args.mongod_port),
+                         '--log', local_log_fn,
+                         '--mongod-from-to', '%d-%d' % (slice_from, slice_to)
+        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        logging.info('worker %s paused', worker_id)
+
+    pool = threadpool.ThreadPool(args.worker_count)
+
+    jobs = []
+    total_docs = get_doc_count()
+    for i in range(0, total_docs, args.worker_job_count):
+        jobs.append([i, min(i+args.worker_job_count, total_docs)])
+
+    logging.info('%d jobs ==> %d workers', len(jobs), args.worker_count)
+    reqs = threadpool.makeRequests(process, jobs)
+    [pool.putRequest(req) for req in reqs]
+    pool.wait()
+    logging.info('all workers stopped')
