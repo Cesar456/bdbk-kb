cf736c4c0fc517995ff820aaa7102f83ab209330
Haoyan Huo <hhaoyann@gmail.com>
2015-07-09 12:47:39 +0800

    change settings.py for static files

--- project/project/settings.py
+++ project/project/settings.py
@@ -1,5 +1,9 @@
 import os
 
+# before turning off this switch:
+# 1. make sure you have managed all static files
+DEBUG=True
+
 if os.environ.has_key('NLP_DEBUG'):
     print 'Enter debug mode for database.'
     DATABASES = {
@@ -21,5 +25,18 @@ else:
     }
 
 SECRET_KEY = '_mw2k_abk#x97bc#_w*fb%&k#uw9_3*63@3kzl(2hb)!lbi8j$'
-INSTALLED_APPS = ['bdbk', 'zhwiki', 'processor']
+INSTALLED_APPS = [
+    'django.contrib.staticfiles',
+    'bdbk',
+    'zhwiki',
+    'processor',
+    'ui'
+]
 MIDDLEWARE_CLASSES = []
+
+ALLOWED_HOSTS = ['localhost']
+ROOT_URLCONF = 'project.urls'
+
+STATIC_URL = '/static/'
+# set this to the static file directory you want to serve in a production server
+# STATIC_ROOT =
