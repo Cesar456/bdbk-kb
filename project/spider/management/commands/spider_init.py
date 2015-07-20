d5391bc88f46a5200da388c6c3aee64dc67116dd
Haoyan Huo <hhaoyann@gmail.com>
2015-07-20 15:55:40 +0800

    add spider init command

---
+++ project/spider/management/commands/spider_init.py
@@ -0,0 +1,9 @@
+import scrapy
+from django.core.management.base import BaseCommand, CommandError
+from spider.models import SpiderEntry
+
+class Command(BaseCommand):
+    help = 'init the spider with http://baike.baidu.com/view/1.htm'
+
+    def handle(self, *args, **options):
+        SpiderEntry.objects.get_or_create(url='http://baike.baidu.com/view/1.htm')
