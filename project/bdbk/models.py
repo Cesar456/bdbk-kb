eaadac3aca30e690980697f785bc74b53361ddc1
Haoyan Huo <hhaoyann@gmail.com>
2015-07-23 16:21:50 +0800

    fix a bug

--- project/bdbk/models.py
+++ project/bdbk/models.py
@@ -13,6 +13,14 @@ from .page_extractor import extractor as page_extractor
 # version: 11
 DBVersion = '0.11'
 
+class BigAutoField(models.fields.AutoField):
+    def db_type(self, connection):
+        if 'mysql' in connection.__class__.__module__:
+            return 'bigint AUTO_INCREMENT'
+        elif 'postgresql' in connection.__class__.__module__:
+            return 'bigserial'
+        return super(BigAutoField, self).db_type(connection)
+
 class BigForeignKey(models.ForeignKey):
     def db_type(self, connection):
         """ Adds support for foreign keys to big integers as primary keys.
@@ -21,16 +29,8 @@ class BigForeignKey(models.ForeignKey):
         if (isinstance(rel_field, BigAutoField) or
                 (not connection.features.related_fields_match_type and
                 isinstance(rel_field, (BigIntegerField, )))):
-            return BigIntegerField().db_type(connection=connection)
+            return models.BigIntegerField().db_type(connection=connection)
         return super(BigForeignKey, self).db_type(connection)
-
-class BigAutoField(models.fields.AutoField):
-    def db_type(self, connection):
-        if 'mysql' in connection.__class__.__module__:
-            return 'bigint AUTO_INCREMENT'
-        elif 'postgresql' in connection.__class__.__module__:
-            return 'bigserial'
-        return super(BigAutoField, self).db_type(connection)
 
 class Category(models.Model):
     '''
