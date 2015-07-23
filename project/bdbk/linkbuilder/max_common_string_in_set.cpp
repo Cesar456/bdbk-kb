d37824a52e8f04b0f3baf94f2d3c09bc1771a421
Haoyan Huo <hhaoyann@gmail.com>
2015-07-23 09:09:03 +0800

    linkbuilder

---
+++ project/bdbk/linkbuilder/max_common_string_in_set.cpp
@@ -0,0 +1,136 @@
+#include<cwchar>
+#include<string>
+#include<iostream>
+#include<map>
+#include<utility>
+
+
+class InversedDict{
+  wchar_t chr;
+  bool isroot;
+  std::wstring *value;
+  std::map<wchar_t, InversedDict*> submaps;
+public:
+  InversedDict(){
+    this->chr = 0;
+    this->isroot = true;
+    this->value = NULL;
+  }
+
+  InversedDict(wchar_t chr, bool isroot=true){
+    this->chr = chr;
+    this->isroot = isroot;
+    this->value = NULL;
+  }
+
+  void set(std::wstring key, std::wstring value){
+    if(key.size()==0){
+      this->value = new std::wstring(value);
+    }else{
+      wchar_t chr = key[0];
+      auto search = this->submaps.find(chr);
+      if(search != this->submaps.end())
+        search->second->set(key.substr(1), value);
+      else{
+        InversedDict* d = new InversedDict(chr, false);
+        d->set(key.substr(1), value);
+        this->submaps[chr] = d;
+      }
+    }
+  }
+
+  std::wstring* get(std::wstring key){
+    if(key.size()==0){
+      if(this->isroot)
+        return NULL;
+
+      return this->value;
+    }else{
+      wchar_t chr = key[0];
+      auto search = this->submaps.find(chr);
+      if(search != this->submaps.end())
+        return search->second->get(key.substr(1));
+      else
+        return NULL;
+    }
+  }
+
+  void match(std::wstring& key, size_t pos, std::wstring** out_value, size_t* out_end, bool root=true){
+    if(root){
+      *out_end = pos;
+      *out_value = NULL;
+    }
+    if(pos >= key.size()){
+      return;
+    } else {
+      wchar_t chr = key[pos];
+      auto search = this->submaps.find(chr);
+      if(search != this->submaps.end()){
+        search->second->match(key, pos+1, out_value, out_end, false);
+        if(*out_value == NULL){
+          *out_value = search->second->value;
+          *out_end = pos+1;
+        }
+      } else {
+        return;
+      }
+    }
+  }
+
+};
+
+int main(){
+  InversedDict dict;
+
+  std::wstring line;
+  while(true){
+    std::getline(std::wcin, line);
+    if(!line.size())
+      break;
+
+    size_t pos = line.find(L'\t');
+    if(pos == std::wstring::npos){
+      std::cerr<<"No tab char found in input"<<std::endl;
+      return -1;
+    }
+
+    std::wstring
+      key(line, 0, pos),
+      value(line, pos+1);
+
+    dict.set(key, value);
+  }
+
+  while(true){
+    std::getline(std::wcin, line);
+    if(!line.size())
+      break;
+
+    std::wstring* output_str;
+    std::wstring nolink_part;
+
+    for(size_t pos=0;pos<line.size();){
+      size_t output_pos = pos;
+
+      dict.match(line, pos, &output_str, &output_pos);
+      if(output_str==NULL){
+        nolink_part.push_back(line[pos]);
+        pos+=1;
+      } else {
+        if(nolink_part.size()){
+          std::wcout<<nolink_part<<std::endl;
+          nolink_part.clear();
+        }
+        std::wcout<<line.substr(pos, output_pos-pos)<<'\t'<<*output_str<<std::endl;
+        pos = output_pos;
+      }
+    }
+    if(nolink_part.size()){
+      std::wcout<<nolink_part<<std::endl;
+      nolink_part.clear();
+    }
+    std::wcout<<std::endl;
+  }
+
+  return 0;
+}
