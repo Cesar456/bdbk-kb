662fca345b8db3218f1d846ccb5b6d2145e02c23
Haoyan Huo <hhaoyann@gmail.com>
2015-08-25 14:05:22 +0800

    qa demo

--- project/bdbk/static/bdbk/js/qa.js
+++ project/bdbk/static/bdbk/js/qa.js
@@ -1,8 +1,124 @@
 $(function(){
+  var hints = ['IBM中国研究院什么时候成立的？',
+               '请告诉我谁是IBM中国研究院的现任院长？',
+               '我想知道IBM全球有多少员工？',
+               '颐和园的门票多少钱？',
+               '大雁塔位于哪里？',
+               '苹果公司的员工数量是多少？',
+               '英国的首都是什么？',
+               '中华人民共和国的国土面积是多少？',
+               '哈佛大学的现任校长是谁？',
+               '周杰伦的妻子是谁？',
+               'Macbook的操作系统是什么？',
+               'Windows的最新版本？',
+               'Quora的创始人是谁？',
+               '太阳的表面温度有多高？',
+               '火星的半径有多大？',
+               '红高粱的导演是谁？',
+               '百度的董事长是？'];
+
+  $('.help-box').mousedown(function(e){
+    var that = $(this);
+    that.data('dragging', true);
+    that.data('ix', e.clientX-this.offsetLeft);
+    that.data('iy', e.clientY-this.offsetTop);
+  }).mousemove(function(e){
+    var that = $(this);
+    if(!that.data('dragging'))
+      return;
+    var oX = e.clientX - that.data('ix');
+    var oY = e.clientY - that.data('iy');
+    that.css({"left":oX + "px", "top":oY + "px"});
+  }).mouseup(function(e){
+    var that = $(this);
+    that.data('dragging', false);
+  });
+
+  $('.help-box .row button').on('countdown', function(e, fn, timeout){
+    var that = $(this);
+    if(that.data('countdown'))
+      return;
+    that.data('countdown', timeout || 5);
+
+    var btn_text = that.text();
+
+    function _doCountDown(){
+      var time = that.data('countdown');
+      if(time == 0){
+        that.text(btn_text);
+        fn();
+      }else{
+        time -= 1;
+        that.data('countdown', time);
+        that.text(btn_text+' ('+time+')');
+        setTimeout(_doCountDown, 1000);
+      }
+    }
+    _doCountDown();
+  }).on('click', function(){
+    var selection = $('.help-box .row select');
+    var option = selection.val();
+    $('#question').trigger('typeeffect', [hints[option]]);
+    if(option != hints.length-1){
+      selection.val(parseInt(option, 10)+1);
+    }
+  });
+
+  $('.help-box').on('demobegin', function(){
+    var that = $(this);
+    if(that.data('demobegin'))
+      return;
+
+    that.data('demobegin', true);
+    that.show();/*
+    $('.help-box .row button').trigger('countdown', [function(){
+      var that = $(this);
+      var selection = $('.help-box .row select');
+      var option = selection.val();
+
+      $('#question').trigger('typedemo', [hints[option], function(){
+
+      }]);
+
+      alert(option);
+    }]);*/
+  });
+
+  $('#demo-qa').click(function(){
+    $('.help-box').trigger('demobegin');
+  });
+
+  (function(){
+    var selects = $('.help-box .row select');
+    hints.forEach(function(q, i){
+      selects.append('<option value="'+i+'">'+q+'</option>');
+    });
+  })();
+
   $('#question').on('keydown', function(e){
     if(e.keyCode==13){
       $('#ask').trigger('click');
     }
+  }).on('typeeffect', function(e, content, fn){
+    var that = $(this);
+    that.val('');
+    var pos = 0;
+
+    function doType(){
+      that.val(that.val() + content[pos++]);
+      if(pos >= content.length){
+        setTimeout(function(){
+          var e = $.Event("keydown");
+          e.keyCode = 13;
+          that.trigger(e);
+          if(fn)
+            setTimeout(fn, 500);
+        }, 500);
+      }
+      else
+        setTimeout(doType, 70);
+    }
+    setTimeout(doType, 500);
   });
   $('#ask').on('click', function(){
     $(this).children('span')
